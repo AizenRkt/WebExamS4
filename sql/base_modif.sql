ce8defdec2368a76f1136351fb54b33517e3a7f5
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2025-07-07 15:11:42 +0300

    feat:base utilisateur

---
+++ sql/base_modif.sql
@@ -0,0 +1,30 @@
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(100) NOT NULL,  -- 'administrateur'
+    description TEXT
+);
+
+CREATE TABLE utilisateur (
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    telephone VARCHAR(50),
+    adresse VARCHAR(255),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_role INTEGER NOT NULL,
+    FOREIGN KEY (id_role) REFERENCES role(id)
+);
+
+
+CREATE TABLE type_transaction (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL,
+    taux_interet DOUBLE PRECISION,
+    id_categorie INTEGER NOT NULL,
+    description TEXT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_categorie) REFERENCES categorie (id)
+);
+
