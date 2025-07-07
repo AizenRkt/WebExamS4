9205d239851048ff74356693cffc774de7fbe3c7
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2025-07-07 15:25:15 +0300

    feat:utilisateur base

--- sql/base_modif.sql
+++ sql/base_modif.sql
@@ -17,6 +17,29 @@ CREATE TABLE utilisateur (
     FOREIGN KEY (id_role) REFERENCES role(id)
 );
 
+CREATE TABLE type_client (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL,
+    description TEXT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE client (
+    id INTEGER PRIMARY KEY,
+    id_type_client INTEGER NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_categorie INTEGER NOT NULL,
+    FOREIGN KEY (id_type_client) REFERENCES type_client (id),
+    FOREIGN KEY (id_categorie) REFERENCES categorie (id)
+);
+
+
+
 
 CREATE TABLE type_transaction (
     id INTEGER PRIMARY KEY,
