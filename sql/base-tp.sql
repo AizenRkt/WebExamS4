6689944c51c2061ab5faa11f65362a2950a3da5b
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2025-07-07 11:56:25 +0300

    feat:init conception base

--- sql/base-tp.sql
+++ sql/base-tp.sql
@@ -1,51 +1,71 @@
-CREATE DATABASE supermarche;
-USE supermarche;
-
-CREATE TABLE caisse (
-    id_caisse INT AUTO_INCREMENT PRIMARY KEY,
-    numero_caisse INT NOT NULL UNIQUE
+CREATE TABLE type_client (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL, -- entreprise, particulier...
+    description TEXT,
+    date DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE produit (
-    id_produit INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE categorie (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL, -- investisseur, debiteur
+    description TEXT,
+    date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE client (
+    id INTEGER PRIMARY KEY,
+    id_type_client INTEGER NOT NULL,
     nom VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    nb_stock INT NOT NULL,
-    prix DECIMAL(15,2) NOT NULL
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    id_categorie INTEGER NOT NULL,
+    FOREIGN KEY (id_type_client) REFERENCES type_client (id),
+    FOREIGN KEY (id_categorie) REFERENCES categorie (id)
 );
 
-CREATE TABLE employe (
-    id_employe INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    mdp VARCHAR(16) NOT NULL
+CREATE TABLE type_transaction (
+    id INTEGER PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL,
+    taux_interet DOUBLE PRECISION,
+    id_categorie INTEGER NOT NULL,
+    description TEXT,
+    date DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (id_categorie) REFERENCES categorie (id)
 );
 
-CREATE TABLE enregistrement (
-    id_enregistrement INT AUTO_INCREMENT PRIMARY KEY,
-    id_produit INT NOT NULL,
-    id_caisse INT NOT NULL,
-    quantite INT NOT NULL CHECK (quantite > 0),
-    validation TINYINT(1) NOT NULL DEFAULT 0,
-    date_achat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_produit) REFERENCES produit(id_produit) ON DELETE CASCADE,
-    FOREIGN KEY (id_caisse) REFERENCES caisse(id_caisse) ON DELETE CASCADE
+CREATE TABLE transaction (
+    id INTEGER PRIMARY KEY,
+    date DATE DEFAULT CURRENT_DATE,
+    montant DOUBLE PRECISION,
+    id_type_transaction INTEGER NOT NULL,
+    id_client INTEGER NOT NULL,
+    FOREIGN KEY (id_client) REFERENCES client (id),
+    FOREIGN KEY (id_type_transaction) REFERENCES type_transaction (id)
 );
 
--- Insertion des caisses
-INSERT INTO caisse (numero_caisse) VALUES 
-(1), 
-(2), 
-(3);
+-- validation ou refus
+CREATE TABLE status_transaction (
+    id INTEGER PRIMARY KEY,
+    id_transaction INTEGER NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    status SMALLINT,
+    FOREIGN KEY (id_transaction) REFERENCES transaction (id)
+);
 
--- Insertion des produits
-INSERT INTO produit (nom, description, nb_stock, prix) VALUES
-('Pomme', 'Fruit rouge ou vert', 100, 1.50),
-('Banane', 'Riche en potassium', 120, 1.20),
-('Lait 1L', 'Lait entier en brique', 50, 1.80),
-('Pain', 'Pain baguette frais', 80, 1.00),
-('Riz 1kg', 'Riz basmati', 40, 2.50),
-('Huile 1L', 'Huile de tournesol', 60, 3.00),
-('Sucre 1kg', 'Sucre en poudre', 70, 1.70),
-('Café 500g', 'Café moulu arabica', 30, 5.00),
-('Jus d\'orange 1L', 'Pur jus sans sucre ajouté', 55, 2.20),
-('Chocolat 100g', 'Tablette de chocolat noir 70%', 90, 2.00);
+-- historique du transaction
+CREATE TABLE detail_transaction (
+    id INTEGER PRIMARY KEY,
+    id_transaction INTEGER NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    montant DOUBLE PRECISION,
+    FOREIGN KEY (id_transaction) REFERENCES transaction (id)
+);
+
+CREATE TABLE compte (
+    id INTEGER PRIMARY KEY,
+    montant DOUBLE PRECISION,
+    type SMALLINT
+);
